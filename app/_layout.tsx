573f9153f441e0e986d64819d8ce0ade9bee6243
Staffan Pedersen <staffanbp@hotmail.com>
2024-09-14 20:27:20 +0200

    testing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Slot, Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <AuthSessionProvider >
+    <AuthSessionProvider>
       <Slot />
     </AuthSessionProvider>
   );
